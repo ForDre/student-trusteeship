1b9ad14528252670ae73eb9599fb2fef3637f6c4
LostConst <lyllost@outlook.com>
2021-12-02 23:29:28 +0800

    upload github

---
+++ app.js
@@ -0,0 +1,26 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null //这是一个全局变量
+  },
+
+  onPageNotFound(res){
+    wx.switchTab({
+      url: 'pages/index/index'
+    })
+  }
+
+})
